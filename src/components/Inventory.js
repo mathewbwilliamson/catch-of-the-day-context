cb6b0301103f115b6903430fa56a66b80e742b77
Mathew Williamson <mwilliamson@64labs.com>
2018-12-23 18:09:35 -0500

    Started AddFishForm and create a state

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import AddFishForm from './AddFishForm'
 
 class Inventory extends React.Component {
     state = {  }
     render() { 
-        return ( <div className='inventory'>Inventory</div> );
+        return ( 
+            <div className='inventory'>
+                <h2>Inventory</h2>
+                <AddFishForm />
+            </div> 
+        );
     }
 }
  
