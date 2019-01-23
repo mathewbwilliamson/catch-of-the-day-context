f6d83fde4a1de5e0d9d3ddca76fe2f0f78de98b8
Mathew Williamson <mwilliamson@64labs.com>
2019-01-23 08:41:25 -0500

    Added Auth, still working on cleaning Auth up

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,29 +1,57 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import AddFishForm from './AddFishForm'
 import EditFishForm from './EditFishForm'
+import Login from './Login'
+import {firebaseApp} from '../base'
+import firebase from 'firebase'
 
+// [matt]: Stopped at 14 minutes
 class Inventory extends React.Component {
+    static propTypes = {
+        fishes: PropTypes.object,
+        updateFish: PropTypes.func,
+        deleteFish: PropTypes.func,
+        addFish: PropTypes.func,
+        loadSampleFishes: PropTypes.func
+    }
+
+    authHandler = async (authData) => {
+        console.log('[matt] authData', authData)
+        
+    }
+
+    authenticate = (provider) => {
+        const authProvider = new firebase.auth[`${provider}AuthProvider`]()
+        firebaseApp
+            .auth()
+            .signInWithPopup(authProvider)
+            .then(this.authHandler)
+        
+    }
+
     render() { 
-        return ( 
-            <div className='inventory'>
-                <h2>Inventory</h2>
-                {Object.keys(this.props.fishes).map(key => 
-                    <EditFishForm 
-                        key={key} 
-                        index={key}
-                        fish={this.props.fishes[key]} 
-                        updateFish={this.props.updateFish} 
-                        deleteFish={this.props.deleteFish}
-                    />
-                )}
-                <AddFishForm 
-                    addFish={this.props.addFish} 
-                />
-                <button onClick={this.props.loadSampleFishes}>
-                    Load Sample Fishes
-                </button>
-            </div> 
-        );
+        return <Login authenticate={this.authenticate}/>
+        // return ( 
+        //     <div className='inventory'>
+        //         <h2>Inventory</h2>
+        //         {Object.keys(this.props.fishes).map(key => 
+        //             <EditFishForm 
+        //                 key={key} 
+        //                 index={key}
+        //                 fish={this.props.fishes[key]} 
+        //                 updateFish={this.props.updateFish} 
+        //                 deleteFish={this.props.deleteFish}
+        //             />
+        //         )}
+        //         <AddFishForm 
+        //             addFish={this.props.addFish} 
+        //         />
+        //         <button onClick={this.props.loadSampleFishes}>
+        //             Load Sample Fishes
+        //         </button>
+        //     </div> 
+        // );
     }
 }
  
