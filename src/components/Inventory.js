6cf609f5a33edc8b7bd467d45b6a34322e77d097
Mathew Williamson <mwilliamson@64labs.com>
2019-01-26 01:55:27 -0500

    Finished Adding Auth with google Firebase

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import AddFishForm from './AddFishForm'
 import EditFishForm from './EditFishForm'
 import Login from './Login'
-import {firebaseApp} from '../base'
+import base, {firebaseApp} from '../base'
 import firebase from 'firebase'
 
 // [matt]: Stopped at 14 minutes
@@ -16,9 +16,45 @@ class Inventory extends React.Component {
         loadSampleFishes: PropTypes.func
     }
 
+    state = {
+        uid: null,
+        owner: null,
+        loading: false
+    }
+
+    // TODO: Can I create a loading circle or icon? How does that work? 
+
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.setState({
+                    loading: true
+                })
+                this.authHandler({ user })
+            }
+        })
+    }
+
     authHandler = async (authData) => {
-        console.log('[matt] authData', authData)
+        // 1. Look up the current store in the firebase database
+        const store = await base.fetch(this.props.storeId, { context : this })
+        console.log('[matt] store', store)
         
+
+        // 2. If there is no owner, claim it
+        if (!store.owner) {
+            // Save it as our own
+            await base.post(`${this.props.storeId}/owner`, {
+                data: authData.user.uid
+            })
+        }
+        // 3. Set the state of the Inventory component to reflect the current user 
+        this.setState({
+            uid: authData.user.uid,
+            owner: store.owner || authData.user.uid,
+            loading: false
+        })
+        console.log('[matt] authData', authData)        
     }
 
     authenticate = (provider) => {
@@ -30,28 +66,54 @@ class Inventory extends React.Component {
         
     }
 
+    logout = async () => {
+        console.log('[matt] Logging Out')
+        await firebase.auth().signOut()
+        this.setState({
+            uid: null,
+        })
+    }
+
     render() { 
-        return <Login authenticate={this.authenticate}/>
-        // return ( 
-        //     <div className='inventory'>
-        //         <h2>Inventory</h2>
-        //         {Object.keys(this.props.fishes).map(key => 
-        //             <EditFishForm 
-        //                 key={key} 
-        //                 index={key}
-        //                 fish={this.props.fishes[key]} 
-        //                 updateFish={this.props.updateFish} 
-        //                 deleteFish={this.props.deleteFish}
-        //             />
-        //         )}
-        //         <AddFishForm 
-        //             addFish={this.props.addFish} 
-        //         />
-        //         <button onClick={this.props.loadSampleFishes}>
-        //             Load Sample Fishes
-        //         </button>
-        //     </div> 
-        // );
+        const logout = <button onClick={this.logout}>Log Out!</button>
+
+        // 1. Check if they are logged in
+        if (!this.state.loading && !this.state.uid) {
+            return <Login authenticate={this.authenticate}/>
+        }
+
+        // 2. Check if they are not the owner of that store
+        if (this.state.uid !== this.state.owner) {
+            return (
+                <div>
+                    <p>Sorry, you are not the owner.</p>
+                    {logout}
+                </div>
+            )
+        }
+
+        // 3. They must be the owner
+        return ( 
+            <div className='inventory'>
+                <h2>Inventory</h2>
+                {logout}
+                {Object.keys(this.props.fishes).map(key => 
+                    <EditFishForm 
+                        key={key} 
+                        index={key}
+                        fish={this.props.fishes[key]} 
+                        updateFish={this.props.updateFish} 
+                        deleteFish={this.props.deleteFish}
+                    />
+                )}
+                <AddFishForm 
+                    addFish={this.props.addFish} 
+                />
+                <button onClick={this.props.loadSampleFishes}>
+                    Load Sample Fishes
+                </button>
+            </div> 
+        );
     }
 }
  
